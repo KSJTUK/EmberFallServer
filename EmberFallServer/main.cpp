0002af6836f4bf200be89d1217db6d8086ae3116
KSJTUK <lastbabo2480@tukorea.ac.kr>
2024-11-02 13:12:48 +0900

    소켓 에러 처리 기능 작성

--- EmberFallServer/main.cpp
+++ EmberFallServer/main.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
+#include "utils/Utils.h"
 
 int main(int argc, char* argv[])
 {
     std::cout << "Hello World" << std::endl;
+
+    ErrorHandle::WSAErrorMessageBox("Error MessageBox Test");
 }
